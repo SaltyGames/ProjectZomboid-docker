f792c378f59de37c0cdaa137d6b89ab40d36a0c4
SaltyGames <salty_games@outlook.com>
2016-01-25 20:17:42 +0000

    Update pz_docker.sh

--- pz_docker.sh
+++ pz_docker.sh
@@ -1,1 +1,1 @@
-docker run -it -p 16261 -p 8766 -p 8767 -p 16262-16272 -v /root/ProjectZomboid-docker/files/Zomboid:/home/steam/Zomboid pz:latest
+docker run -it -p 16261 -p 8766 -p 8767 -p 16262-16272 -v ./files/Zomboid:/home/steam/Zomboid pz:latest
